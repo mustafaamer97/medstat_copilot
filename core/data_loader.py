1f445f5d0cc5cd690ef0b49210617b1649fddd2d
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-09-21 21:37:46 +0300

    Create data_loader.py

---
+++ core/data_loader.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def load_file(uploaded_file):
+
+    if uploaded_file.name.endswith(".csv"):
+        return pd.read_csv(uploaded_file)
+
+    elif uploaded_file.name.endswith(".xlsx"):
+        return pd.read_excel(uploaded_file)
+
+    raise ValueError("Unsupported file format")
